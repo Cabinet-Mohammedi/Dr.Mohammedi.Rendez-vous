f7bf711e863d71bcaae773fe40dc5c48e78ab59d
Cabinet-Mohammedi <cabinet.mohammedi17@gmail.com>
2025-11-09 16:47:42 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+﻿// firebase-config.js (type=module)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBIrVOglgZALaaK6IwPwqHMiynBGD4Z3JM",
+  authDomain: "mohammedi-cabinet.firebaseapp.com",
+  databaseURL: "https://mohammedi-cabinet-default-rtdb.firebaseio.com",
+  projectId: "mohammedi-cabinet",
+  storageBucket: "mohammedi-cabinet.appspot.com",
+  messagingSenderId: "666383356275",
+  appId: "1:666383356275:web:09de11f9dfa2451d843506"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db };
